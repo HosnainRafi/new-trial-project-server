74c7340dd9e4905dd2c575f3991f23b2f0c461f0
Your <you@example.com>
2023-03-16 02:16:48 +0600

    First day done

--- index.js
+++ index.js
@@ -93,8 +93,9 @@ app.listen(port, () => {
 
 
 
-const express = require("express");
+/* const express = require("express");
 const mongodb = require("mongodb");
+const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const { validationResult } = require("fluent-validation");
@@ -222,4 +223,123 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log('Port is Ok');
+}) */
+
+
+
+const express = require("express");
+const mongodb = require("mongodb");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config(); //dotenv config
+const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+
+const { MongoClient, ServerApiVersion, ObjectID } = require('mongodb');
+const uri = `mongodb+srv://new-trial-project:rafi1234@cluster0.1uzcg7w.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    await client.connect();
+
+    app.post("/api/employees", async (req, res) => {
+      try {
+        // Add a new Employee object to the 'employees' collection
+        const employee = req.body;
+        const db = client.db();
+        const result = await db.collection("employees").insertOne(employee);
+
+        // Return success status and URL of created object
+        res.status(201).location(`/api/employees/${result.insertedId}`).send();
+      } catch (err) {
+        console.error(err);
+        res.status(500).send("Internal server error");
+      }
+    });
+
+    app.get("/api/employees/:id", async (req, res) => {
+      try {
+        // Find an Employee object with the given ID in the 'employees' collection
+        const db = client.db();
+        const employee = await db.collection("employees")
+          .findOne({ _id: ObjectID(req.params.id) });
+
+        if (!employee) {
+          return res.status(404).send("Not found");
+        }
+        res.json(employee);
+      } catch (err) {
+        console.error(err);
+        res.status(500).send("Internal server error");
+      }
+    });
+
+    app.put("/api/employees/:id", async (req, res) => {
+      try {
+        // Find an Employee object with the given ID in the 'employees' collection
+        const db = client.db();
+        const employee = await db.collection("employees").findOne({ _id: ObjectID(req.params.id) });
+
+        if (!employee) {
+          return res.status(404).send("Not found");
+        }
+
+        // Update the Employee object with data from request body
+        const updatedEmployee = { ...employee, ...req.body };
+
+        // Save the updated Employee object to the 'employees' collection
+        await db.collection("employees").updateOne(
+          { _id: ObjectID(req.params.id) },
+          { $set: updatedEmployee }
+        );
+
+        // Return success status and URL of updated object
+        res.location(`/api/employees/${req.params.id}`).send();
+      } catch (err) {
+        console.error(err);
+        res.status(500).send("Internal server error");
+      }
+    });
+
+
+
+    app.delete("/api/employees/:id", async (req, res) => {
+      try {
+        // Find and delete an Employee object with the given ID from the 'employees' collection
+        const db = client.db();
+        const result = await db.collection("employees").deleteOne({ _id: ObjectID(req.params.id) });
+
+        // If no Employee object was deleted, return 404 status code
+        if (result.deletedCount === 0) {
+          return res.status(404).send("Not found");
+        }
+
+        // Return success status
+        res.send();
+      } catch (err) {
+        console.error(err);
+        res.status(500).send("Internal server error");
+      }
+    });
+
+
+  } finally {
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Server is ok')
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
 })
